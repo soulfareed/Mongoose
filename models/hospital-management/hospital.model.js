5eef0307ad210b1ceb0247d723a7955f0ef57643
Md fareed <fareedzaman501@gmail.com>
2024-10-31 06:31:21 +0530

    hospital management model comp

--- models/hospital-management/hospital.model.js
+++ models/hospital-management/hospital.model.js
@@ -1,5 +1,31 @@
+import { RequiredValidator } from "@angular/forms";
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({},{timestamps: true})
+const hospitalSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    addressLine :{
+        type: String,
+        required: true
+    },
+    addressLine2:{
+        type: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: String,
+        required: true
+    },
+    specializedIn :[
+        {
+            type: String
+        }
+    ]
+},{timestamps: true})
 
 export const Hospital = mongoose.model("Hospital", hospitalSchema)
--- models/hospital-management/medical_record.model.js
+++ models/hospital-management/medical_record.model.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
-const medicalRecordSchema = new mongoose.Schema({}, {timestamps: true})
+const medicalRecordSchema = new mongoose.Schema({
+    name: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient"
+    },
+    disease: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
 
 export const MedicalRecord = mongoose.model("MedicalRecord", medicalRecordSchema)
